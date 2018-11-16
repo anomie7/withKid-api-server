72edac35558d86f985e4d198af1d9d887978b532
Justin <anomie7777@gmail.com>
2018-11-16 18:44:01 +0900

    [Impl] EventLogController에 findLastEventLogs 메소드 추가

--- src/main/java/com/withkid/api/web/controller/EventLogController.java
+++ src/main/java/com/withkid/api/web/controller/EventLogController.java
@@ -1,8 +1,12 @@
 package com.withkid.api.web.controller;
 
+import java.beans.EventSetDescriptor;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -18,14 +22,18 @@ public class EventLogController {
 	@Autowired
 	private EventLogService eventLogService;
 	
-	@Autowired
-	private JwtService jwtService;
+	@GetMapping("/eventLog")
+	public ResponseEntity<Set<EventLogDto>> findLatestEventLogs(@RequestHeader(name="Authorization") String accessToken){
+		String key = eventLogService.getKey(accessToken);
+		Set<EventLogDto> body = eventLogService.findLast10Logs(key);
+		return ResponseEntity.ok().body(body);
+	}
 	
 	@PostMapping("/eventLog")
 	public ResponseEntity<String> addEventLog(@RequestHeader(name="Authorization") String accessToken,
 											  @RequestBody EventLogDto eventLog) {
-		String key = jwtService.getKey(accessToken);
-		eventLogService.save(key, eventLog);
+		eventLogService.saveByAccessToken(accessToken, eventLog);
 		return ResponseEntity.ok().body("log stored");
 	}
+	
 }
