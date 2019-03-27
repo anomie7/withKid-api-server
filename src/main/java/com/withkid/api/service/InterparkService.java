748f43e6be50cb63a3b947d60cc4103ce1ea704e
Justin <anomie7777@gmail.com>
2019-03-28 01:32:23 +0900

    [Impl] 반환받은 event들을 ulog-server로 부터 입력받은 id들의 순서대로  재정렬해서 반환하는 findAllByIds 메소드 구현

--- src/main/java/com/withkid/api/service/InterparkService.java
+++ src/main/java/com/withkid/api/service/InterparkService.java
@@ -1,25 +1,26 @@
 package com.withkid.api.service;
 
-import java.util.List;
-
+import com.google.common.collect.Lists;
+import com.withkid.api.domain.InterParkContent;
+import com.withkid.api.dto.SearchVO;
+import com.withkid.api.repository.InterParkRepository;
+import com.withkid.api.repository.InterparkPredicateProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.google.common.collect.Lists;
-import com.withkid.api.domain.InterParkContent;
-import com.withkid.api.dto.SearchVO;
-import com.withkid.api.repository.InterParkRepository;
-import com.withkid.api.repository.InterparkPredicateProvider;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class InterparkService {
 	
 	@Autowired
 	private InterParkRepository interparkRepository;
-	
+
 	@Transactional(readOnly=true)
 	public Page<InterParkContent> searchEvent(SearchVO search, Pageable pageable) {
 		Page<InterParkContent> event = interparkRepository.findAll(InterparkPredicateProvider.getSearchPredicate(search)
@@ -33,4 +34,12 @@ public class InterparkService {
 		return Lists.newArrayList(event);
 	}
 
+	@Transactional(readOnly=true)
+	public List<InterParkContent> findAllByIds(List<Long> ids) {
+		Iterable<InterParkContent> event = interparkRepository.findAll(InterparkPredicateProvider.getEventPredicate(ids));
+		List<InterParkContent> result = ids.stream().map(id ->
+				Lists.newArrayList(event).stream().filter(content -> content.getId().equals(id)).findFirst().orElse(null)
+		).filter(Objects::nonNull).collect(Collectors.toList());
+		return result;
+	}
 }
